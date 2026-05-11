87865f407333e97c63810add0fd7c7e5abdaa008
Gideon Mohr <mohr@cs.uni-saarland.de>
2026-05-11 11:49:29 +0200

    fix: RoundedFloatingPointToIntegerTest

--- src/test/scala/circuits/floatingpoint/public/bonus/rounding/RoundedFloatingPointToIntegerTest.scala
+++ src/test/scala/circuits/floatingpoint/public/bonus/rounding/RoundedFloatingPointToIntegerTest.scala
@@ -4,59 +4,58 @@ import chisel3._
 import chisel3.simulator.scalatest.ChiselSim
 import org.scalatest.flatspec.AnyFlatSpec
 
-import sysarch.circuits.floatingpoint.bonus.nBit.nBitFloatingPointToInteger
+import sysarch.circuits.floatingpoint.bonus.rounding.RoundedFloatingPointToInteger
 import sysarch.circuits.util._
 
 class RoundedFloatingPointToIntegerTest extends AnyFlatSpec with ChiselSim {
 
   private def pokeFloat32(
       vec: chisel3.Vec[chisel3.Bool],
-      value: Double,
-      exponentWidth: Int,
-      mantissaWidth: Int
+      value: Double
   ): Unit = {
-    val bits = FloatingPoint.doubleToIEEE754(value, exponentWidth, mantissaWidth)
-    for (i <- 0 until (1 + exponentWidth + mantissaWidth)) {
+    val bits = FloatingPoint.doubleTo32BitIEEE754(value)
+    for (i <- 0 until 32) {
       vec(i).poke(bits(i).B)
     }
   }
 
   private def expectSignedMagnitude(
       vec: chisel3.Vec[chisel3.Bool],
-      expected: BigInt,
-      integerWidth: Int
+      expected: BigInt
   ): Unit = {
-    val expectedBits = SignedMagnitude.bigIntToSignedMagnitude(expected, integerWidth)
-    for (i <- 0 until integerWidth) {
+    val expectedBits = SignedMagnitude.bigIntToSignedMagnitude(expected, 32)
+    for (i <- 0 until 32) {
       vec(i).expect(
         if (expectedBits(i)) true.B else false.B,
         s"Bit $i did not match expected value for input $expected.\n" +
-          s"Expected ${(for (i <- 0 until integerWidth) yield if (expectedBits(i)) "1" else "0").mkString} \n" +
-          s"but got  ${(for (i <- 0 until integerWidth)
+          s"Expected ${(for (i <- 0 until 32) yield if (expectedBits(i)) "1" else "0").mkString} \n" +
+          s"but got  ${(for (i <- 0 until 32)
               yield if (vec(i).peek().litToBoolean) "1" else "0").mkString}.\n"
       )
     }
   }
 
   val testInputs = Seq(
-    (8, 23, 32, 1.0, 1),
-    (5, 10, 16, -1.0, -1),
-    (32, 32, 32, 0.0, 0),
-    (8, 32, 64, -1.25, -1),
-    (5, 10, 16, -1.5, -1),
-    (5, 10, 16, -1.75, -1),
-    (5, 10, 16, 0.75, 0),
-    (5, 10, 16, 0.5, 0),
-    (5, 10, 16, 0.25, 0),
-    (8, 23, 4, 2147483648.0, 7)
+    (1.0, 1),
+    (-1.0, -1),
+    (0.0, 0),
+    (-1.25, -1),
+    (-1.5, -2),
+    (-1.75, -2),
+    (0.75, 1),
+    (0.5, 0),
+    (1.5, 2),
+    (2.5, 2),
+    (0.25, 0),
+    (0.49, 0)
   )
 
-  for ((exponentWidth, mantissaWidth, integerWidth, floatValue, intValue) <- testInputs) {
+  for ((floatValue, intValue) <- testInputs) {
     it should s"convert $floatValue to $intValue" in {
-      simulate(new nBitFloatingPointToInteger(exponentWidth, mantissaWidth, integerWidth)) { dut =>
-        pokeFloat32(dut.floatInput, floatValue, exponentWidth, mantissaWidth)
+      simulate(new RoundedFloatingPointToInteger) { dut =>
+        pokeFloat32(dut.floatInput, floatValue)
         dut.clock.step()
-        expectSignedMagnitude(dut.intOutput, intValue, integerWidth)
+        expectSignedMagnitude(dut.intOutput, intValue)
       }
     }
   }
