fc5900921da22d2d352877ed67e2b7f3c3e8f687
Marco Simon Zillgen <marcosimonzillgen@gmail.com>
2026-05-12 20:29:57 +0200

    feat: 2.1 (SignedMagnitudeToOnesComplement)

--- src/main/scala/circuits/integer/SignedMagnitudeToOnesComplement.scala
+++ src/main/scala/circuits/integer/SignedMagnitudeToOnesComplement.scala
@@ -14,4 +14,17 @@ class SignedMagnitudeToOnesComplement(width: Int) extends Module {
     - a_(n-1) == 1: we invert a_(n-2) to a_0 and copy a into b
    */
 
+  val len     = width - 1
+  val signBit = signedMagnitude(len)
+
+  val mux = Module(new Mux(len))
+  for (i <- 0 until len) {
+    mux.a(i) := signedMagnitude(i)
+    mux.b(i) := Not(signedMagnitude(i))
+  }
+  mux.sel := signBit
+
+  for (i <- 0 until width) {
+    onesComplement(i) := mux.out(i)
+  }
 }
