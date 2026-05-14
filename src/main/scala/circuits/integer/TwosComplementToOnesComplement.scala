d9886a52b8a30dff523da40722e5c32256879d01
Sanskar Pathak <sapa00011@stud.uni-saarland.de>
2026-05-14 14:53:55 +0000

    feat: 2.2 and 2.4 (TwosComplementToOnesComplement and SignedMagnitudeToTwosComplement) Untested due to dependency

--- src/main/scala/circuits/integer/TwosComplementToOnesComplement.scala
+++ src/main/scala/circuits/integer/TwosComplementToOnesComplement.scala
@@ -8,5 +8,16 @@ class TwosComplementToOnesComplement(width: Int) extends Module {
   val twosComplement = IO(Input(Vec(width, Bool())))
   val onesComplement = IO(Output(Vec(width, Bool())))
 
-  ???
+  val adder = Module(new nBitAdderSubtractor(width)) {
+    adder.a          := twosComplement
+    adder.b          := ((true.B) +: Vec.fill(width - 1)(false.B))
+    adder.enable_sub := true.B
+  }
+  val res = Module(new MUX(width)) {
+    res.a   := twosComplement
+    res.b   := adder.sum
+    res.sel := twosComplement(width - 1)
+  }
+  onesComplement := res.out
+
 }
